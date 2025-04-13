ce1d8ede5ab8e8b4f63f04bd4dc91ab9826b1618
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-04-13 09:13:13 +0200

    docs: install dotenv-webapck plugin to support environment variables

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 // webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,6 +14,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/template.html',
     }),
+    new Dotenv(),
   ],
   module: {
     rules: [
