d897a9bcde58ecd517443daff349cfdd0605810e
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-04-15 21:11:48 +0200

    feat: implement data method to render data in the DOM

---
+++ src/js/core/utils/data.js
@@ -0,0 +1,37 @@
+import fetchCoordinates from '../fetch/geocode.js';
+import fetchTodaysData from '../fetch/fetchCurrentForecast.js';
+import fetchFiveDayData from '../fetch/fetchForecast.js';
+import formatFiveDayForecast from '../utils/formatFiveDayForecast.js';
+import RenderUI from '../ui/renderUI.js';
+import Notify from '../ui/notify.js';
+
+export default async function data(query) {
+  const coordinates = await fetchCoordinates(query);
+
+  if (!coordinates) {
+    Notify.error('Failed to fetch any location', 1500);
+    return;
+  }
+
+  const weatherToday = await fetchTodaysData(
+    coordinates.latitude,
+    coordinates.longitude
+  );
+
+  RenderUI.summary(weatherToday);
+  console.log(weatherToday);
+
+  const weather = await fetchFiveDayData(
+    coordinates.latitude,
+    coordinates.longitude
+  );
+
+  console.log('Next 24 hrs:');
+  console.log(weather.next24hrs);
+
+  console.log('Five-day forecast:');
+
+  const fiveDayForecast = formatFiveDayForecast(weather.nextFivedays);
+
+  console.log(fiveDayForecast);
+}
