c35454f48c021a2ebbc10235c38cf929e14e6ac3
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-04-18 09:34:32 +0200

    feat: enhance Units.toggle to support location fetch based on units toggle button

--- src/js/ui/units.js
+++ src/js/ui/units.js
@@ -1,14 +1,19 @@
+import retrieveLocation from '../core/utils/retrieveLocation';
+
 export default class Units {
   static toggle() {
     const btn = document.querySelector('#metrics-toggle');
     const main = document.querySelector('#main');
+    const location = document.querySelector('#location').textContent;
 
     if (btn.textContent === '°C') {
       btn.textContent = '°F';
       main.setAttribute('data-metrics', 'imperial');
+      retrieveLocation(location);
     } else {
       btn.textContent = '°C';
       main.setAttribute('data-metrics', 'metric');
+      retrieveLocation(location);
     }
   }
 
